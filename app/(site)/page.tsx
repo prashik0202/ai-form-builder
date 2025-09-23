cc37ddaff996d9333b3dfc335c3cda854672e8b6
Prashik <gamreprashik03@gmail.com>
2025-09-23 12:36:09 +0530

    final commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const Home = () => {
   return (
@@ -8,7 +9,11 @@ const Home = () => {
       <div className='p-10 flex flex-col gap-5'>
         <h2 className='text-5xl md:text-7xl lg:text-8xl text-purple-500'>FormGenAi</h2>
         <p className='text-lg md:text-xl lg:text-2xl text-muted-foreground'>Generate Form with AI with support of shadcn ui + Zod validation.</p>
-        <Button className='w-fit' size={"lg"}>Get Started</Button>
+        <Button className='w-fit' size={"lg"} asChild>
+          <Link href={"/genform"}>
+            Get Started
+          </Link>
+        </Button>
       </div>
     </div>
   );
